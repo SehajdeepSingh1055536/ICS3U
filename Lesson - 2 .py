44d81432bc7053fb9e899f7fb689e9f0cc01736b
SehajdeepSingh1055536 <1055536@pdsb.net>
2024-09-19 10:39:10 -0400

    Create Lesson - 2 .py

---
+++ Lesson - 2 .py
@@ -0,0 +1,24 @@
+#modify 1 
+amount = 24
+print("Two dozen is: ",amount)
+Two dozen is:  24
+
+
+#modify 2 
+amount2 = 7*4
+print("forty-two is: ", amount2)
+forty-two is =  28
+
+#modify 3 
+amount = 8
+amount = amount + 1
+print("The amount is: ", amount)
+# answer = 9
+# it add the amount 1 into 8 but when we use = the code doesn't work.
+
+#modify 4 
+amount = 8
+amount = amount * 2
+print(amount)
+# the answer = 16
+# same the code doesn't work when we use =
