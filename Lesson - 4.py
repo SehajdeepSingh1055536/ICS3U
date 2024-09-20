83df3fef7c260a330d4a1d74837e82c658eac3e4
SehajdeepSingh1055536 <1055536@pdsb.net>
2024-09-20 09:24:36 -0400

    Create Lesson - 4.py

---
+++ Lesson - 4.py
@@ -0,0 +1,13 @@
+num = input("Please input a whole number: ") 
+num = int(num) 
+num2 = num / 6
+print("When", num, " is divided by 6", "the result is:", num2)
+# it didn't give any number it just output the sentence.
+
+
+num = (12)
+num = int(num) 
+num2 = num / 6
+print("When", num, " is divided by 6", "the result is:", num2)
+
+# now the code is worked the answer is When 12  is divided by 6 the result is: 2.0.
