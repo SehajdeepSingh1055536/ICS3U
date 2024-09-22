d2d0e6c38d07653182fa894820ba5a5c1f4cb984
SehajdeepSingh1055536 <1055536@pdsb.net>
2024-09-22 15:37:51 -0700

    Create Lesson - 5.py

---
+++ Lesson - 5.py
@@ -0,0 +1,7 @@
+import math
+length = 12
+length = float(length)
+area = math.pow(length, 2)
+print("The area of a square of side length", length, "is:", area)
+#The area of a square of side length 12.0 is: 144.0
+# it didn't work before adding 12 
