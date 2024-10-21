be1899db36a03b19a35be04802afc2f55129c1da
SehajdeepSingh1055536 <1055536@pdsb.net>
2024-10-21 19:48:31 -0400

    Update Unit-3 Lesson-5.py

--- Unit-3 Lesson-5.py
+++ Unit-3 Lesson-5.py
@@ -1,4 +1,34 @@
 def add(a, b):
+  return a + b
+
+print(add(7, 2))
+# prediction - by adding the "7" and "2" the output would be 9. By replacing the the "7" and "2" with strings of any value the function will still work. But we mix data types the code will be crash.
+
+# modify 1 
+def add(a, b):
+    return a + b
+
+# Getting user input
+input1 = input("Enter the first number: ")
+input2 = input("Enter the second number: ")
+
+# Checking if both inputs are numerical
+try:
+    # Try to convert inputs to float
+    num1 = float(input1)
+    num2 = float(input2)
+    
+    # Calling the function and multiplying the result by 2
+    result = add(num1, num2) * 2
+    print("The result is:", result)
+    
+except ValueError:
+    # If conversion fails, inform the user
+    print("Please enter valid numerical values (int or float).")
+    
+#modify - 2 
+
+def add(a, b):
     return a + b
 
 def myPow(m, n):
@@ -23,7 +53,7 @@ try:
     print("The result of myPow is:", my_pow_result)
     
     # Checking against math.pow()
-    math_pow_result = math.pow(base, exponent)
+   math_pow_result = math.pow(base, exponent)
     print("The result of math.pow is:", math_pow_result)
     
     # Verifying if both results match
