8dbece3b5f3d6dda47d9fd149b55394559b2f179
SehajdeepSingh1055536 <1055536@pdsb.net>
2024-11-27 09:45:47 -0500

    Update u5L1p2.py

--- u5L1p2.py
+++ u5L1p2.py
@@ -6,3 +6,13 @@ for i in range(3):
   t.forward(100)
   t.left(120)
   # this code will end up making triangle.
+
+# modify
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(60):
+  t.forward(100)
+  t.left(123)
+# The code work but it has 3 degree error. instead of making triangle the lines start making another lines because of 3 degree error.
