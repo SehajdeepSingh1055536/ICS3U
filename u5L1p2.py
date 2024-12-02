66a3a2b86775a265e81ae7c5e3226725052f4ea2
SehajdeepSingh1055536 <1055536@pdsb.net>
2024-12-02 08:27:27 -0500

    Update u5L1p2.py

--- u5L1p2.py
+++ u5L1p2.py
@@ -16,3 +16,17 @@ for i in range(60):
   t.forward(100)
   t.left(123)
 # The code work but it has 3 degree error. instead of making triangle the lines start making another lines because of 3 degree error.
+
+# modify2
+import turtle
+t = turtle.Turtle()
+for i in range(80):
+    t.forward(100)
+    t.left(93) # Slight over-turn to create a spiral
+    
+    
+import turtle
+t = turtle.Turtle()
+for i in range(80):
+    t.forward(100)
+    t.left(87) # Slight under-turn to create a spiral 
